051aded63695cc60c185b2827a3ad6d066a62d49
Byron Ajin <byronap120@gmail.com>
2019-05-16 21:05:46 -0600

    Adding preference fragment

--- app/src/main/java/com/example/xoomnavigation/home/ActivityFragment.kt
+++ app/src/main/java/com/example/xoomnavigation/home/ActivityFragment.kt
@@ -2,23 +2,21 @@ package com.example.xoomnavigation.home
 
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.xoomnavigation.R
 
-class RecipientsFragment : Fragment() {
+
+class ActivityFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipients, container, false)
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.main_menu, menu)
+        return inflater.inflate(R.layout.fragment_activity, container, false)
     }
 
 
--- app/src/main/java/com/example/xoomnavigation/home/HomeActivity.kt
+++ app/src/main/java/com/example/xoomnavigation/home/HomeActivity.kt
@@ -1,7 +1,13 @@
 package com.example.xoomnavigation.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.example.xoomnavigation.R
 
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +15,35 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        super.onCreateOptionsMenu(menu)
+//        menuInflater.inflate(R.menu.main_menu, menu)
+//        return true
+//        //return retValue
+//    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //return super.onOptionsItemSelected(item)
+        // TODO STEP 9.2 - Have Navigation UI Handle the item selection - make sure to delete
+        //  the old return statement above
+//        // Have the NavigationUI look for an action or destination matching the menu
+//        // item id and navigate there if found.
+//        // Otherwise, bubble up to the parent.
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 }
